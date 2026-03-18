6fffafdb05a59605627d3f8ba3f40e2f5b3fc3bf
NoahShinar <noahshinar@gmail.com>
2026-03-18 13:06:15 -0400

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 
+// University Library Management System
+/*
+Write a System in C++ to manage books and magazines in a University. The university library is
+accessible to students, faculty and staff. Each person borrowing a book from the library is
+assigned a maximum duration in which the loaner either has to return the book or request an
+extension of 10 days at a time. All persons must register with the library before starting to
+borrow books. Students are allowed to borrow a maximum of 5 books at a time, while faculty
+and staff can borrow up to 10 books.
+
+Books in the library are organized according to the Library of Congress Book classification.
+Follow the following article for more information:
+https://www.loc.gov/catdir/cpso/lcco/
+https://en.wikipedia.org/wiki/Library_of_Congress_Classification
+
+The Library system needs to:
+• Manage lending and returns of books and magazines
+• Review and display Members account o List borrowed books
+• List borrowed magazines
+• Request an extension to a borrowed books
+• Adding and removing Members to the library
+• Adding and removing books to the library
+• Handle fines of $1.75 per day on late books.
+ */
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
