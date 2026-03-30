861261173f3b25c4eec093893d0a866833bf751c
kiwanski07 <ciwan1@unh.newhaven.edu>
2026-03-30 13:40:26 -0400

    first two coding portions for checkpoint 1

--- Accounts.cpp
+++ Accounts.cpp
@@ -1,5 +1,36 @@
 //
-// Created by noahs on 3/24/26.
+// Coded by Kami I. on 3/30/36.
 //
 
 #include "Accounts.h"
+#include <iostream>
+
+using namespace std;
+
+//
+string Accounts::borrowedBooks()
+{
+    return "No books stored";
+}
+
+string Accounts::borrowedMagazines()
+{
+    return "No magazines stored";
+}
+
+int Accounts::maxBorrowTime()
+{
+    if (type == "Student")
+    {
+        return 30; //days loaned
+    }
+    else if (type == "Faculty" || type == "Staff")
+    {
+        return 60; //days loaned
+    }
+    else
+    {
+        return 30; //default
+    }
+}
+
--- Book.cpp
+++ Book.cpp
@@ -1,5 +1,82 @@
 //
-// Created by noahs on 3/24/26.
+// Coded by Kami I. on 3/30/26.
 //
 
 #include "Book.h"
+#include <iostream>
+
+using namespace std;
+
+//book info and such
+void displayBook (Book b)
+{
+  cout << "Title: " << b.title << endl;
+  cout << "Author: " << b.author << endl;
+  cout << "Classification: " << b.classification << endl;
+  cout << "Year: " << b.yearPublished << endl;
+  cout << "Borrowed Status: " << (b.isBorrowed? "Borrowed" : "Still available") << endl;
+}
+
+//book borrowing section
+void borrowBook (Book &b)
+{
+    if (!b.isBorrowed)
+    {
+     b.isBorrowed = true;
+        cout << "Book successfully borrowed\n";
+    }
+    else
+    {
+        cout << "Book already borrowed\n";
+    }
+}
+
+//book returns (NOT MAGAZINES!!!)
+void returnBook (Book &b, int currentDay, int currentMonth, int currentYear)
+{
+ b.isBorrowed = false;
+
+ int lateDays = currentDay - b.dueDay;
+
+ if (lateDays > 0)
+ {
+  double fine = lateDays * 1.75;
+     cout << "Late return. Calculated fine: $" << fine << endl;
+ }
+ else
+ {
+  cout << "Returned on time!\n";
+ }
+}
+
+//setting the due date for the books
+void setDueDate (Book &b, int day, int month, int year, string accountType)
+{
+ int borrowDays;
+
+ if (accountType == "Student")
+ {
+  borrowDays = 30;
+ }
+ else if (accountType == "Faculty" || accountType == "Staff")
+ {
+  borrowDays = 60;
+ }
+ else
+ {
+  borrowDays = 30; //default
+ }
+    b.dueDay = day + borrowDays;
+    b.dueMonth = month;
+    b.dueYear = year;
+}
+
+//extending the person's due date
+void extendDueDate (Book &b)
+{
+ b.dueDay += 10;
+    cout << "Due date extended by 10 days. Enjoy!\n";
+}
+
+
+
