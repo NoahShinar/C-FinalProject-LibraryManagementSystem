a09564f7050246987787767e1b3cc34c843594a4
kiwanski07 <ciwan1@unh.newhaven.edu>
2026-04-20 13:15:18 -0400

    ATE.cpp addition

--- AccountTypeEnum.cpp
+++ AccountTypeEnum.cpp
@@ -1,5 +1,18 @@
 //
-// Created by noahs on 4/15/26.
+// Coded by Kami I. on 4/15/26.
 //
 
 #include "AccountTypeEnum.h"
+#include <string>
+
+using namespace std;
+
+string AccountTypeToString(AccountTypeEnum type)
+{
+    if (type == AccountTypeEnum::STUDENT)
+        return "STUDENT";
+    else if (type == AccountTypeEnum::FACULTY)
+        return "FACULTY";
+    else if (type == AccountTypeEnum::STAFF)
+        return "STAFF";
+}
