d3549a12eaa20eb686657ad6d0c6ed09c52fbdee
NoahShinar <noahshinar@gmail.com>
2026-04-24 21:30:39 -0400

    fixes

--- FileHandler.h
+++ FileHandler.h
@@ -22,6 +22,7 @@ class FileHandler {
     friend class Library;
     friend class Book;
     friend class Magazine;
+
     private:
         const string ACCOUNTS_FILE = "/home/noahs/CLionProjects/FinalProject/RegisteredAccounts.txt";
         const string BOOKS_FILE = "/home/noahs/CLionProjects/FinalProject/Books.txt";
