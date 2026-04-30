379b687cbb5fa118162bb2d21b68b72135e21b25
NoahShinar <noahshinar@gmail.com>
2026-04-30 12:16:24 -0400

    facility -> faculty

--- main.cpp
+++ main.cpp
@@ -83,8 +83,8 @@ int main() {
         cout << endl;
         cout << "1. add new account" << endl; // Done
         cout << "2. remove account" << endl; // Done
-        cout << "3. add book or magazine to the library (staff and facility only)" << endl; // Done
-        cout << "4. remove book or magazine from the library (staff and facility only)" << endl; // Done
+        cout << "3. add book or magazine to the library (staff and faculty only)" << endl; // Done
+        cout << "4. remove book or magazine from the library (staff and faculty only)" << endl; // Done
         cout << "5. borrow book or magazine" << endl; // Done
         cout << "6. return book or magazine" << endl; // Done
         cout << "7. view account" << endl; // Done
